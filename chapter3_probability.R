4b6688c8351e34fe5e982d171284cada22c2aaa8
harryyang1982 <harry.seunghoon.yang@gmail.com>
2022-03-18 14:11:25 +0900

    dfd

--- chapter3_probability.R
+++ chapter3_probability.R
@@ -10,4 +10,14 @@ mean(x)
 x <- rbinom(10000, 1, 0.5)
 mean(x)
 
-k = ""
+## 시뮬레이션
+
+n_simulation <- 1000000
+n_success <- 0
+for(i in 1:n_simulation){
+  x <- sample(1:5, 3, replace=FALSE)
+  if(x[1] >= 4) n_success <-  n_success + 1
+  if((x[1] == 3) & (x[2] >= 2)) n_success <- n_success + 1
+}
+
+n_success / n_simulation
