68a2e73722eff37b806ebdc3031f49b965e4f050
harryyang1982 <harry.seunghoon.yang@gmail.com>
2022-03-20 22:09:34 +0900

    finalize

--- chapter5_prob_distribution.R
+++ chapter5_prob_distribution.R
@@ -27,3 +27,24 @@ y <- rbinom(n_sim, 100, 0.5)
 hist(y, xlab='X', ylab='mass', main='Binom(100, 0.5)', prob=TRUE, breaks=30)
 curve(dnorm(x, 50, 5), 25, 75, add=T, lty=2, lwd=1, col='red')
 
+# 남녀 키를 통해 본 중심극한 정리
+n_sim <- 100000
+n <- 30
+means <- vector(length = n_sim)
+
+for (i in 1:n_sim){
+  y <- vector(length=n) # 길이가 n인 저장소를 만든다
+  for (j in 1:30) { # 30명을 추출한다
+    gender <- rbinom(1, 1, 0.5) # 개인별로 성별을 추출한다 
+    if (gender == 0) { # 추출된 성별이 여성이라면
+      y[j] <- rnorm(1, 160, 5) # 여성 집단에서 키를 추출
+    }
+    else { # 추출된 성별이 남성이라면 
+      y[j] <- rnorm(1, 175, 5) # 남성 집단에서 키를 추출
+    }
+  }
+  means[i] <- mean(y)
+}
+
+hist(means, xlab='Mean_Height', ylab = 'prob', main = 'Distribution of means', prob = TRUE)
+0
